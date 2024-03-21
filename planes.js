bf3de3779ce35429ca57ab818fea9e3b5779f09c
Sara <sara_campos@estudante.sesisenai.org.br>
2024-03-21 12:13:35 -0300

    Add plane settings

--- planes.js
+++ planes.js
@@ -2,23 +2,26 @@ const container = document.querySelector('footer .container');
 
 const planes = [
     plane1 = {
-        name: '',
-        description: '',
-        price: '',
-        img: '',
+        name: 'Airbus',
+        price: 1000,
+        img: './assets/ap-1.webp',
     },
 
     plane2 = {
-        name: '',
-        description: '',
-        price: '',
-        img: '',
+        name: 'Boeing',
+        price: 2000,
+        img: './assets/ap-2.webp',
     },
 
     plane3 = {
-        name: '',
-        description: '',
-        price: '',
-        img: '',
-    }
+        name: 'Cessna Centurion',
+        price: 3000,
+        img: './assets/ap-3.webp',
+    },
+
+    plane4 = {
+        name: 'Cessna',
+        price: 4000,
+        img: './assets/ap-4.webp',
+    },
 ];
