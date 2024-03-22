f5e4ce1779f1e4c36d430479f43ef736ae1d56e5
Miguel Zacca <miguel_zacca@estudante.sesisenai.org.br>
2024-03-21 22:21:14 -0300

    Adding store.js and initial money system

--- products.js
+++ products.js
@@ -3,25 +3,25 @@ const container = document.querySelector('footer .container');
 const planes = [
     plane1 = {
         name: 'Airbus',
-        price: 1000,
+        price: 1100,
         img: './assets/ap-1.webp',
     },
 
     plane2 = {
         name: 'Boeing',
-        price: 2000,
+        price: 3200,
         img: './assets/ap-2.webp',
     },
 
     plane3 = {
-        name: 'Cessna Centurion',
-        price: 3000,
-        img: './assets/ap-3.webp',
+      name: 'Cessna Centurion',
+      price: 11000,
+      img: './assets/ap-3.webp',
     },
 
     plane4 = {
         name: 'Cessna',
-        price: 4000,
+        price: 5500,
         img: './assets/ap-4.webp',
     },
 ];
@@ -29,7 +29,7 @@ const planes = [
 planes.forEach((plane) => {
   container.innerHTML += `
     <div class="spawn">
-     <img src=${plane.img} alt=${plane.name}>
+     <img src=${plane.img} class="hover" alt="${plane.name}">
      <h2>${plane.name}</h2>
      <button>
        ${plane.price}
---
+++ store.js
@@ -0,0 +1,79 @@
+const products = document.querySelectorAll(".container div");
+const products_btn = document.querySelectorAll(".container div button");
+const products_img = document.querySelectorAll(".container div img.hover");
+const storeMoney = localStorage.getItem("money") || 0;
+
+const updateMoney = () => {
+  document.querySelector("header").innerHTML = `
+    ${storeMoney.toString().padStart(5, "0")}
+    <img src="./assets/icon.png" />
+  `;
+}
+
+updateMoney();
+
+const checkMoney = () => {
+  products.forEach((product, i) => {
+    if (planes[i].price > localStorage.getItem("money") && !localStorage.getItem(i)) {
+      product.style.filter = 'brightness(.7) grayscale(1)';
+      products_btn[i].disabled = true;
+      products_btn[i].style.cursor = 'not-allowed';
+      products_img[i].removeAttribute("class");
+    }
+  });
+}
+
+checkMoney();
+
+products.forEach((product, i) => {
+  if (localStorage.getItem(i)) {
+    products_btn[i].innerHTML = 'Select';
+  }
+});
+
+products_btn.forEach((btn, i) => {
+  const src = `.${products_img[i].src.split("AeroDash")[1]}`;
+
+  btn.addEventListener("click", () => {
+    const check = () => {
+      products_btn.forEach((btn) => {
+        if (btn.innerHTML === '✓') {
+          if (localStorage.getItem(i)) {
+            btn.innerHTML = 'Select';
+          }
+        }
+      });
+
+      if (btn.innerHTML === 'Select') {   
+        products_btn.forEach((btn) => {
+          if (btn.innerHTML === '✓') {
+            btn.innerHTML = 'Select';
+          }
+        });
+        btn.innerHTML = '✓';
+        localStorage.removeItem("plane");
+      }
+
+      if (localStorage.getItem(i) && localStorage.getItem("plane") === src) {
+        btn.innerHTML = 'Select';
+        localStorage.removeItem("plane");
+      } else if (localStorage.getItem(i)) {
+        localStorage.setItem("plane", src);
+        btn.innerHTML = '✓';
+      }
+    }
+
+    if (planes[i].price <= localStorage.getItem("money")) {
+      localStorage.setItem(i, true);
+      localStorage.setItem("money", Number(localStorage.getItem("money") - planes[i].price));
+    }
+
+    check();
+    checkMoney();
+    updateMoney();
+  });
+
+  if (localStorage.getItem("plane") === src) {
+    btn.innerHTML = '✓';
+  }
+});
